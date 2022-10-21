a50dcf7df807903b01374af01525181f228b98d3
Keith S Thomas <kthomasnj@hotmail.com>
2022-10-21 19:48:40 -0400

    Added Signin functionality

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -1,19 +1,29 @@
 const router = require('express').Router();
+const withAuth = require('../utils/auth');
 
 router.get("/", (req, res) => {
     res.render('homepage');
 });
 
-router.get("/dashboard", (req, res) => {
-    res.render('dashboard');
-});
+router.get('/dashboard', withAuth, async (req, res) => {
+
+    res.render('dashboard', {
+      logged_in: true
+    });
+ 
+  }
+);
 
 router.get("/login", (req, res) => {
     res.render('login');
 });
 
-router.get("/signup", (req, res) => {
-    res.render('signup');
-});
+router.get('/signup', async (req, res) => {
+    try {
+      res.render('signup');
+    } catch (err) {
+      res.status(500).json(err);
+    }
+  });
 
 module.exports = router;

b2f892fd0ec4dd08ae388093bfe3974b97ee275c
Keith S Thomas <kthomasnj@hotmail.com>
2022-10-21 20:58:43 -0400

    Added logic to send post information to homepage:

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -1,8 +1,27 @@
 const router = require('express').Router();
+const { Posts, User } = require('../models');
 const withAuth = require('../utils/auth');
 
-router.get("/", (req, res) => {
-    res.render('homepage');
+router.get("/", async (req, res) => {
+    try {
+        const postData = await Posts.findAll({
+            include: [
+                {
+                model: User,
+                attributes: ['Name'],
+                },
+            ],
+        });
+
+        const posts = postData.map((project) => this.post.get({ plain: true }));
+
+        res.render('homepage', {
+            posts,
+            logged_in: req.session.logged_in
+        });
+    } catch (err) {
+        res.status(500).json(err);
+    }
 });
 
 router.get('/dashboard', withAuth, async (req, res) => {

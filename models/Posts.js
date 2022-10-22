6ed45f98972675a155cff7cf6a5731137eb142f9
Keith S Thomas <kthomasnj@hotmail.com>
2022-10-21 20:47:29 -0400

    Added post routes

--- models/Posts.js
+++ models/Posts.js
@@ -19,10 +19,13 @@ Posts.init(
         type: DataTypes.STRING,
         allowNull: false
       },
-      author: {
-        type: DataTypes.STRING,
-        allowNull: false
-      }
+      user_id: {
+        type: DataTypes.INTEGER,
+        references: {
+          model: 'user',
+          key: 'id',
+        }
+      },
     },
     {
       sequelize,

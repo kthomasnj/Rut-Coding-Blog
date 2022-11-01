09927882f598425f8ed3ad1e6cb5a4920827f01e
Keith S Thomas <kthomasnj@hotmail.com>
2022-11-01 11:33:01 -0400

    Updated post update functionality

--- public/js/update.js
+++ public/js/update.js
@@ -2,16 +2,12 @@ async function updatePost() {
     const title = document.querySelector('#title').value;
     const postText = document.querySelector('#postContent').value;
     const postId = window.location.href.substring(window.location.href.lastIndexOf('/') + 1);
-    const author = document.querySelector('#currentUser').value;
-
-    console.log(author);
 
     const response = await fetch('/api/update/'+ postId, {
         method: 'PUT',
         body: JSON.stringify({
             title,
             postText,
-            author: author,
         }),
         headers: {
             'Content-Type': 'application/json',

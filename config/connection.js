d732931282d60e4f95515f00a210309dbdc6a80e
Keith S Thomas <kthomasnj@hotmail.com>
2022-11-01 23:15:36 -0400

    Added Posgres db

--- config/connection.js
+++ config/connection.js
@@ -3,15 +3,25 @@ require('dotenv').config();
 
 let sequelize;
 
-sequelize = new Sequelize(
+if (process.env.DATABASE_URL) {
+  sequelize = new Sequelize(process.env.DATABASE_URL, {dialectOptions: {
+    ssl: {
+        rejectUnauthorized: false
+    }
+}});
+} else {
+  console.log('Using local DB');
+  sequelize = new Sequelize(
     process.env.DB_NAME,
     process.env.DB_USER,
-    process.env.DB_PASSWORD,
+    process.env.DB_PASSWORD,    
     {
       host: 'localhost',
       dialect: 'mysql',
       port: 3306
-    }
+    },
+    process.env.NODE_TLS_REJECT_UNAUTHORIZED = '0',
   );
+}
 
 module.exports = sequelize;
